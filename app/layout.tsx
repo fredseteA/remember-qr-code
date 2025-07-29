78a927bfef138685aaf33a4c03aa9208607b2c54
unknown <fredericoluna93@gmail.com>
2025-07-29 13:18:49 -0300

    forcando o pull request do commit anterior

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
         
         {/* SEO e Open Graph */}
         <title>Remember QR Code - Memorial Online</title>
