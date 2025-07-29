938e3667ac7672722eeee79230b1621221c899f3
unknown <fredericoluna93@gmail.com>
2025-07-29 09:20:11 -0300

    Adicionando logo no link do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,26 @@ export const metadata: Metadata = {
   icons: {
     icon: "/fav-ico.png", // <-- aqui está seu favicon
   },
+    openGraph: {
+    title: "Remember QR Code - Memorial Online",
+    description: 
+      "Crie uma homenagem eterna para quem partiu. Gere um memorial online e receba um QR Code para colar no túmulo ou local especial.",
+    images: [
+      {
+        url: 'https://remember-qr.vercel.app/fav-ico.png', // logo que vai aparecer no link
+        width: 800, // Largura da imagem em pixels
+        height: 600, // Altura da imagem em pixels
+        alt: 'Logo Remember QR Code', 
+      },
+    ],
+  },
+  // PARA TWITTER CARDS TAMBÉM 
+  twitter: {
+    card: 'summary',
+    title: "Remember QR Code - Memorial Online",
+    description: "Crie uma homenagem eterna para quem partiu. Gere um memorial online e receba um QR Code para colar no túmulo ou local especial.",
+    images: ['https://remember-qr.vercel.app/fav-ico.png'],
+  },
 }
 
 export default function RootLayout({
